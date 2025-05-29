443d3078c0b5d9e6b8687530007b442d7147d800
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-05-29 13:16:35 +0300

    Add static seeds to the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.new(email: "aljam3@ieasybooks.com", password: "aljam3@ieasybooks.com", role: :admin).tap(&:skip_confirmation!).save
+
+Library.find_or_create_by(name: "المكتبة الشاملة الوقفية")
+Library.find_or_create_by(name: "المكتبة الوقفية")
+Library.find_or_create_by(name: "مكتبة المسجد النبوي")
