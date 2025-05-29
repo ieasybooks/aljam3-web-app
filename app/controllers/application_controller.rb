dffb04a809e53b522a9ea6ecaad14048bd2a8414
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-05-29 12:36:08 +0300

    Disable modern browsers filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
 
   before_action :check_rack_mini_profiler
 
