86b1dc87eb31a03f437335b1fec136ed2bddb8fa
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-05-29 11:49:48 +0300

    Generate RubyUI ThemeToggle component

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import RubyUi__ThemeToggleController from "./ruby_ui/theme_toggle_controller"
+application.register("ruby-ui--theme-toggle", RubyUi__ThemeToggleController)
