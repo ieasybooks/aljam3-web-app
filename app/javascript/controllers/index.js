4bdcaa967ffa94bfc7831a9f25f56e8ebec5a492
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-06-02 15:53:17 +0300

    Generate RubyUI Carousel component

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,9 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import RubyUi__CarouselController from "./ruby_ui/carousel_controller"
+application.register("ruby-ui--carousel", RubyUi__CarouselController)
+
 import RubyUi__FormFieldController from "./ruby_ui/form_field_controller"
 application.register("ruby-ui--form-field", RubyUi__FormFieldController)
 
