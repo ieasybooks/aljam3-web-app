c75a7e91ce782da5b7d22cfc023359c0f1f13ea3
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-06-04 02:34:02 +0300

    Implement initial search functionality and refinements UI

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,27 @@
 class PagesController < ApplicationController
-  def home = render Views::Pages::Home.new
+  def home
+    results = (search if params[:query].present?)
+
+    render Views::Pages::Home.new(results:)
+  end
+
+  private
+
+  def search
+    query = params[:query]
+    scope = params[:refinements][:search_scope]
+
+    if scope == "title-and-content"
+      Meilisearch::Rails.federated_search(
+        queries: {
+          Book => { q: query },
+          Page => { q: query }
+        }
+      )
+    elsif scope == "title"
+      Book.search(query)
+    elsif scope == "content"
+      Page.search(query)
+    end
+  end
 end
