f9771ca2cab72e38c29d82da6af7812764b77d4b
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-06-04 14:46:28 +0300

    Re-order libraries seeding script

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ require 'faker'
 
 User.new(email: "aljam3@ieasybooks.com", password: "aljam3@ieasybooks.com", role: :admin).tap(&:skip_confirmation!).save
 
-Library.find_or_create_by(name: "المكتبة الشاملة الوقفية")
-Library.find_or_create_by(name: "المكتبة الوقفية")
 Library.find_or_create_by(name: "مكتبة المسجد النبوي")
+Library.find_or_create_by(name: "المكتبة الوقفية")
+Library.find_or_create_by(name: "المكتبة الشاملة الوقفية")
 
 if Book.count == 0
   10.times do
