db7b8797fae63cabd8bb8b04a7a1d50a0c4ce750
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-06-07 12:25:37 +0300

    Remove the next page loading indicator for not
    I can't get the total hits from the federated search, so I can't decide accurately when to show the indicator and when to not show it.

--- app/components/search_results_list.rb
+++ app/components/search_results_list.rb
@@ -34,12 +34,6 @@ class Components::SearchResultsList < Components::Base
             ), loading: :lazy
           end
         end
-
-        if next_page
-          turbo_frame_tag :results_list, next_page do
-            div(class: "flex justify-center mt-4") { loading_spinner }
-          end
-        end
       end
     end
   end
@@ -55,11 +49,4 @@ class Components::SearchResultsList < Components::Base
   end
 
   def results_count = @results.respond_to?(:hits) ? @results.hits.size : @results.size
-
-  def loading_spinner
-    svg(class: "size-5 animate-spin", fill: "none", viewbox: "0 0 24 24") do |s|
-      s.circle(class: "opacity-25", cx: "12", cy: "12", r: "10", stroke: "currentColor", stroke_width: "4")
-      s.path(class: "opacity-75", fill: "currentColor", d: "M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z")
-    end
-  end
 end
