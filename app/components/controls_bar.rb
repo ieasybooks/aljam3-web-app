bfe1c3550103290747709051636e10cad14dadba
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-06-09 14:42:04 +0300

    Create ControlsBar component

---
+++ app/components/controls_bar.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class Components::ControlsBar < Components::Base
+  def initialize(**attrs)
+    super(**attrs)
+  end
+
+  def view_template
+    div(**attrs) do
+      yield
+    end
+  end
+
+  def tooltip(text:, content_data: {}, &)
+    Tooltip do
+      TooltipTrigger do
+        yield
+      end
+
+      TooltipContent(class: "delay-100 max-sm:hidden", data: content_data) do
+        Text { text }
+      end
+    end
+  end
+
+  def button(**attrs, &)
+    attrs[:variant] ||= :outline
+    attrs[:size] ||= :md
+    attrs[:icon] ||= true
+
+    Button(**attrs) do
+      yield
+    end
+  end
+
+  def dummy_button = Button(variant: :link, size: :md, icon: true, class: "max-sm:hidden pointer-events-none")
+
+  private
+
+  def default_attrs = { class: "flex items-center justify-between p-2 gap-x-2 overflow-x-auto rounded-lg bg-gray-100 dark:bg-[#2a2a2e]" }
+end
