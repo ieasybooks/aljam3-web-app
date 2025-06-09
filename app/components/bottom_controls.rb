81c5b0196189393af2ef4b276b53dea227f194d0
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-06-09 14:50:25 +0300

    Disable page controls by default

--- app/components/bottom_controls.rb
+++ app/components/bottom_controls.rb
@@ -16,7 +16,10 @@ class Components::BottomControls < Components::Base
 
   def first_page_button(bar)
     bar.tooltip(text: t(".first_page"), content_data: { bottom_controls_target: "firstPageButtonTooltip" }) do
-      bar.button(data: { action: "click->bottom-controls#firstPage", bottom_controls_target: "firstPageButton" }) do
+      bar.button(
+        data: { action: "click->bottom-controls#firstPage", bottom_controls_target: "firstPageButton" },
+        disabled: "true",
+      ) do
         Hero::ChevronDoubleLeft(variant: :solid, class: "size-5 rtl:transform rtl:-scale-x-100")
       end
     end
@@ -24,7 +27,10 @@ class Components::BottomControls < Components::Base
 
   def previous_page_button(bar)
     bar.tooltip(text: t(".previous_page"), content_data: { bottom_controls_target: "previousPageButtonTooltip" }) do
-      bar.button(data: { action: "click->bottom-controls#previousPage", bottom_controls_target: "previousPageButton" }) do
+      bar.button(
+        data: { action: "click->bottom-controls#previousPage", bottom_controls_target: "previousPageButton" },
+        disabled: "true",
+      ) do
         Hero::ChevronLeft(variant: :solid, class: "size-5 rtl:transform rtl:-scale-x-100")
       end
     end
@@ -32,7 +38,10 @@ class Components::BottomControls < Components::Base
 
   def next_page_button(bar)
     bar.tooltip(text: t(".next_page"), content_data: { bottom_controls_target: "nextPageButtonTooltip" }) do
-      bar.button(data: { action: "click->bottom-controls#nextPage", bottom_controls_target: "nextPageButton" }) do
+      bar.button(
+        data: { action: "click->bottom-controls#nextPage", bottom_controls_target: "nextPageButton" },
+        disabled: "true",
+      ) do
         Hero::ChevronRight(variant: :solid, class: "size-5 rtl:transform rtl:-scale-x-100")
       end
     end
@@ -40,7 +49,10 @@ class Components::BottomControls < Components::Base
 
   def last_page_button(bar)
     bar.tooltip(text: t(".last_page"), content_data: { bottom_controls_target: "lastPageButtonTooltip" }) do
-      bar.button(data: { action: "click->bottom-controls#lastPage", bottom_controls_target: "lastPageButton" }) do
+      bar.button(
+        data: { action: "click->bottom-controls#lastPage", bottom_controls_target: "lastPageButton" },
+        disabled: "true",
+      ) do
         Hero::ChevronDoubleRight(variant: :solid, class: "size-5 rtl:transform rtl:-scale-x-100")
       end
     end
