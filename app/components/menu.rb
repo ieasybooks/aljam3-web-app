a8221e4765fcbf15b7c9b3cb4c0ee4fdafb0e089
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-06-15 16:32:44 +0300

    Show theme trigger component in mobile menu if page is Pages#show

--- app/components/menu.rb
+++ app/components/menu.rb
@@ -3,19 +3,45 @@
 class Components::Menu < Components::Base
   def view_template
     div(class: "pb-4") do
-      menu_link(text: t("navbar.home"), path: root_path)
+      menu_link(path: root_path, text: t("navbar.home"), icon: Hero::Home.new(variant: :outline, class: "size-5"))
+      dark_mode_toggle if controller_name == "pages" && action_name == "show"
     end
   end
 
   private
 
-  def menu_link(text:, path:)
-    a(
+  def menu_link(path:, text:, icon:)
+    Link(
+      variant: :link,
       href: path,
       class: [
-        "group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline",
+        "flex items-center justify-start gap-x-2",
         (path == request.path ? "text-foreground font-medium" : "text-muted-foreground")
       ]
-    ) { text }
+    ) do
+      render icon
+
+      plain text
+    end
+  end
+
+  def dark_mode_toggle
+    ThemeToggle do
+      SetLightMode do
+        Button(variant: :link, class: "flex items-center gap-x-2 text-muted-foreground") do
+          Lucide::Sun(class: "size-5")
+
+          plain t(".light_mode")
+        end
+      end
+
+      SetDarkMode do
+        Button(variant: :link, class: "flex items-center gap-x-2 text-muted-foreground") do
+          Remix::MoonFill(class: "size-5")
+
+          plain t(".dark_mode")
+        end
+      end
+    end
   end
 end
