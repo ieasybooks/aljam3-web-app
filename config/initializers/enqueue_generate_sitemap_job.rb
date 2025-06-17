7cda2636128d48ac90514f52aa1a3776c51f0324
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-06-18 01:57:24 +0300

    Skip enqueue_generate_sitemap_job initializer in Rails consoles

--- config/initializers/enqueue_generate_sitemap_job.rb
+++ config/initializers/enqueue_generate_sitemap_job.rb
@@ -1,5 +1,5 @@
 Rails.application.config.after_initialize do
-  if ENV["DEPLOY"].blank? && Rails.env.production? && ENV.fetch("KAMAL_CONTAINER_NAME", "").start_with?("aljam3-web")
+  if ENV["DEPLOY"].blank? && Rails.env.production? && ENV.fetch("KAMAL_CONTAINER_NAME", "").start_with?("aljam3-web") && !Rails.const_defined?("Console")
     GenerateSitemapJob.perform_later
   end
 end
