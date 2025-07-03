8fd331cc810143a013f4356891ab361307ce7a06
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-07-03 12:53:38 +0300

    Reduce ReindexMeilisearchJob sleep time

--- app/jobs/reindex_meilisearch_job.rb
+++ app/jobs/reindex_meilisearch_job.rb
@@ -4,13 +4,16 @@ class ReindexMeilisearchJob < ApplicationJob
   queue_as :default
 
   def perform(model, start_id, end_id, step = 10000)
+    # TODO: Remove this after reindexing is complete, and use ActiveJob::Continuable when it's released.
+    start_id = 1130000
+
     model_class = model.constantize
 
     start_id.step(end_id, step) do |range_start_id|
       range_end_id = [ range_start_id + step - 1, end_id ].min
       model_class.where(id: range_start_id..range_end_id).reindex!
 
-      sleep 600
+      sleep 300
     end
   end
 end
