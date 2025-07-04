d945bfbc8beca0ec35b856c8fc3b1fd0ce1123b5
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-07-04 22:05:04 +0300

    Enhance cloudflare turnstile controller

--- app/components/cloudflare_turnstile.rb
+++ app/components/cloudflare_turnstile.rb
@@ -1,15 +1,8 @@
 # frozen_string_literal: true
 
 class Components::CloudflareTurnstile < Components::Base
-  def initialize(**attrs)
-    super(**attrs)
-  end
-
-  def view_template
-    div(**attrs) do
-      cloudflare_turnstile
-    end
-  end
+  def initialize(**attrs) = super(**attrs)
+  def view_template = raw cloudflare_turnstile(**attrs)
 
   private
 
--- app/javascript/controllers/cloudflare_turnstile_controller.js
+++ app/javascript/controllers/cloudflare_turnstile_controller.js
@@ -3,8 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="cloudflare-turnstile"
 export default class extends Controller {
   connect() {
-    if (document.querySelector(".cf-turnstile") && window.turnstile) {
-      window.turnstile.render(".cf-turnstile", { language: document.documentElement.lang });
+    if (window.turnstile && this.element.innerHTML.trim() === "") {
+      window.turnstile.render(this.element);
     }
   }
 }
