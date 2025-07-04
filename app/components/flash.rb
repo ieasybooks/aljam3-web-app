f588414a850d856d51fa56893012eff5ba87a208
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-07-04 22:14:03 +0300

    Update flash component margins

--- app/components/flash.rb
+++ app/components/flash.rb
@@ -8,15 +8,15 @@ class Components::Flash < Components::Base
 
         if type == "notice"
           Alert(variant: :success, dismissible: true, class: "mt-4") do
-            Hero::CheckCircle(class: "size-6")
+            Hero::CheckCircle(class: "mt-px size-6")
 
-            AlertDescription(class: "mt-1 me-4") { message }
+            AlertDescription(class: "mt-1.5 me-4") { message }
           end
         elsif type == "alert"
           Alert(variant: :destructive, dismissible: true, class: "mt-4") do
-            Hero::ExclamationTriangle(class: "size-6")
+            Hero::ExclamationTriangle(class: "mt-px size-6")
 
-            AlertDescription(class: "mt-1 me-4") { raw(safe(message)) }
+            AlertDescription(class: "mt-1.5 me-4") { raw(safe(message)) }
           end
         end
       end
