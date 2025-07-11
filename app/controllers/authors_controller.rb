35f63f1eb4e2aef561ae87fd96d2b36ee0274304
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-07-11 23:45:11 +0300

    Cache authors list in user's browser for a week

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,3 +1,7 @@
 class AuthorsController < ApplicationController
-  def index = render json: Author.order(:name).pluck(:id, :name).map { |id, name| { id:, name: } }
+  def index
+    expires_in 1.week, public: true
+
+    render json: Author.order(:name).pluck(:id, :name).map { |id, name| { id:, name: } }
+  end
 end
