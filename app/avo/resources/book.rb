2e6c6a1ab4d62598477cfc240a8a49f4fa197ebe
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-08-16 16:45:09 +0300

    Add views_count to Avo's dashboard for Book model

--- app/avo/resources/book.rb
+++ app/avo/resources/book.rb
@@ -12,6 +12,7 @@ class Avo::Resources::Book < Avo::BaseResource
     field :volumes, as: :number
     field :pages_count, as: :number
     field :files_count, as: :number, readonly: true
+    field :views_count, as: :number, readonly: true
     field :library, as: :belongs_to
     field :files, as: :has_many
     field :pages, as: :has_many, through: :files
