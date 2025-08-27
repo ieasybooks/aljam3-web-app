dceff3bcb0f034b3d63e0e54e871f68147f13272
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-08-27 13:52:15 +0300

    Enhance home page carousels for mobile

--- app/views/static/home.rb
+++ app/views/static/home.rb
@@ -84,12 +84,12 @@ class Views::Static::Home < Views::Base
 
   def most_viewed_books
     div(class: "py-16 sm:py-20 flex items-center") do
-      div(class: "flex-1 border-t border-border")
+      div(class: "flex-1 border-t border-border max-sm:-ms-4")
       Text(size: "9", class: "px-4 text-center font-[Cairo] max-sm:text-3xl") { t(".most_viewed") }
-      div(class: "flex-1 border-t border-border")
+      div(class: "flex-1 border-t border-border max-sm:-me-4")
     end
 
-    Carousel(class: "sm:border-r sm:border-l", options: { direction: }) do
+    Carousel(class: "sm:border-r sm:border-l max-sm:-mx-4", options: { direction: }) do
       CarouselContent(class: "max-sm:group-[.is-horizontal]:-ms-2") do
         Book.most_viewed(10).each do |book|
           CarouselItem(class: "max-sm:basis-xs md:basis-1/2 lg:basis-1/4 max-sm:group-[.is-horizontal]:ps-2") do
@@ -105,9 +105,9 @@ class Views::Static::Home < Views::Base
 
   def books_by_category
     div(class: "py-16 sm:py-20 flex items-center") do
-      div(class: "flex-1 border-t border-border")
+      div(class: "flex-1 border-t border-border max-sm:-ms-4")
       Text(size: "9", class: "px-4 text-center font-[Cairo] max-sm:text-3xl") { t(".discover_books") }
-      div(class: "flex-1 border-t border-border")
+      div(class: "flex-1 border-t border-border max-sm:-me-4")
     end
 
     @carousels_books_ids.call.each_with_index do |(carousel_name, books_ids), index|
@@ -119,7 +119,7 @@ class Views::Static::Home < Views::Base
         ]
       ) { t(".#{carousel_name}") }
 
-      Carousel(class: "sm:border-r sm:border-l", options: { direction: }) do
+      Carousel(class: "sm:border-r sm:border-l max-sm:-mx-4", options: { direction: }) do
         CarouselContent(class: "max-sm:group-[.is-horizontal]:-ms-2") do
           Book.where(id: books_ids).each do |book|
             CarouselItem(class: "max-sm:basis-xs md:basis-1/2 lg:basis-1/4 max-sm:group-[.is-horizontal]:ps-2") do
@@ -136,9 +136,9 @@ class Views::Static::Home < Views::Base
 
   def categories
     div(class: "py-16 sm:py-20 flex items-center") do
-      div(class: "flex-1 border-t border-border")
+      div(class: "flex-1 border-t border-border max-sm:-ms-4")
       Text(size: "9", class: "px-4 text-center font-[Cairo] max-sm:text-3xl") { t(".categories") }
-      div(class: "flex-1 border-t border-border")
+      div(class: "flex-1 border-t border-border max-sm:-me-4")
     end
 
     div(class: "flex justify-center") do
