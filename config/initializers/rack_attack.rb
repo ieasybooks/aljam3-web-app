6015e478742ec17c6c249132f62c94b056c85046
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2025-09-09 10:58:34 +0300

    Add API requests throttling

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,3 +1,7 @@
 Rack::Attack.blocklist("CloudFlare WAF bypass") do |req|
   req.path != "/up" && Rails.env.production? && !req.cloudflare?
 end
+
+Rack::Attack.throttle("API Requests/IP", limit: 60, period: 1.minute) do |req|
+  req.ip if req.path.include?("/api/")
+end
